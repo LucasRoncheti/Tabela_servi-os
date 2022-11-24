c4af1c03279df565d4dbabf0cc5b2937f927d4a8
LucasRoncheti <lucasroncheti@gmail.com>
2022-11-24 09:58:18 -0300

    adicionado fotos de serviços analia modas tldo lucina ranibicke marrecolider padaria

--- js/data.js
+++ js/data.js
@@ -1,30 +1,60 @@
 let data_list=[
     {
     id:"01",
-    cliente:"Cliente 01",
-    serviço:"Fachada acm ",
-    data_entrega:"22/02/22",
-    desc:"faxada de loja em acm cor preto ",
-    img01:"./images/01.jpg",
-    img02:"./images/02.jpg",
-    img03:"./images/",
+    cliente:"Analia modas",
+    serviço:"Fachada acm  ",
+    data_entrega:"30/11/22",
+    desc:"fachada em acm preto com sanca espots de led por baixo  ",
+    img01:"./images/analia.png",
+   
     
 
     },
 
     {
     id:"02",
-    cliente:"Cliente 02",
-    serviço:"telhado policarboneto ",
-    data_entrega:"23/02/22",
-    desc:"-Telhado em policarbonato 4mm com estrutura em perfil galvaniza. <br> -Telhado em policarbonato 4mm com estrutura em perfil galvanizao Telhado em policarbonato 4mm com estrutura em perfil galvanizao <br> Telhado em policarbonato 4mm com estrutura em perfil galvanizao ",
-    img01:"./images/02.jpg"
+    cliente:"Marreco Motos",
+    serviço:"Placa em acm  ",
+    data_entrega:"30/12/22",
+    desc:"Placa em acm azul com letreiro em pvc 10mm branco ",
+    img01:"./images/marreco.png"
     
 
     },
 
     {
     id:"03",
+    cliente:"Lider padaria / Fábio ",
+    serviço:"fachada em acm logo pvc 10mm ",
+    data_entrega:"10/12/22",
+    desc:"fachada em acm bordo e letreiro em pvc 10mm com detalhes pretos ",
+    img01:"./images/lider .jpeg"
+
+    },
+
+    {
+    id:"04",
+    cliente:"Luciana todos os santos ",
+    serviço:"toldo lona e estrutura  ",
+    data_entrega:"02/12/22",
+    desc:"Toldo em lona bege com estrutura em metalon galvanizado ",
+    img01:"./images/toldo luciana.jpeg"
+
+    },
+
+    {
+    id:"05",
+    cliente:"Rani Bicker",
+    serviço:"Placa em acm  ",
+    data_entrega:"10/12/22",
+    desc:"placa em acm medindo 300x65 cm acm branco com letras em pvc 10mm pintado de rosa  ",
+    img01:"./images/RaniBicker.jpeg",
+    img02:"./images/mediasRani.jpeg"
+
+    },
+
+    {
+    id:"06",
     cliente:"Cliente 03",
     serviço:"fachada loja ",
     data_entrega:"25/02/22",
@@ -34,37 +64,7 @@ let data_list=[
     },
 
     {
-    id:"03",
-    cliente:"Cliente 03",
-    serviço:"fachada loja ",
-    data_entrega:"25/02/22",
-    desc:"faxada de loja em acm cor preto ",
-    img01:"./images/03.jpg"
-
-    },
-
-    {
-    id:"03",
-    cliente:"Cliente 03",
-    serviço:"fachada loja ",
-    data_entrega:"25/02/22",
-    desc:"faxada de loja em acm cor preto ",
-    img01:"./images/03.jpg"
-
-    },
-
-    {
-    id:"03",
-    cliente:"Cliente 03",
-    serviço:"fachada loja ",
-    data_entrega:"25/02/22",
-    desc:"faxada de loja em acm cor preto ",
-    img01:"./images/03.jpg"
-
-    },
-
-    {
-    id:"03",
+    id:"07",
     cliente:"Cliente 03",
     serviço:"fachada loja ",
     data_entrega:"25/02/22",
